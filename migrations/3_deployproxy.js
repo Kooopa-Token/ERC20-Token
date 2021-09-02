09ea12e1fd14ffaf5f52f21d2e2a1f45f67fd60e
sumit26696 <team@kooopa.io>
2021-09-02 15:53:11 +0530

    ERC20 contract source code update

---
+++ migrations/3_deployproxy.js
@@ -0,0 +1,8 @@
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+const KooopaToken = artifacts.require("Kooopa");
+
+module.exports = async function(deployer) {
+  const instance = await deployProxy(KooopaToken,[], { deployer: deployer, kind: 'uups', });
+//   const instance = await upgradeProxy("0xf46b919dd731aeee7244fa63849ed0300cb540fa", KooopaToken, [], { deployer: deployer, kind: 'uups', });
+  console.log('deployed', instance)
+};
